64e4cef887b5f29a2b656ad59dd6b0716212f9be
Mikael <mikael.thorman@gmail.com>
2025-01-27 08:49:51 +0100

    adding logs

--- src/npm-aws/install.sh
+++ src/npm-aws/install.sh
@@ -12,6 +12,8 @@ export NPM_CONFIG_USERCONFIG="/.npmrc"
 export AWS_CONFIG_FILE="/.aws/config"
 export AWS_SHARED_CREDENTIALS_FILE="/.aws/credentials"
 
+ECHO "Installing GIT"
 source ./install_git.sh
 
+ECHO "Installing AWS CLI"
 source ./install_aws_cli.sh
